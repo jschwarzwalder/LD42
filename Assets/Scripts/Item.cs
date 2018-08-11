fdb72a3bdc144bce601ce3da41065228437300ec
Jami Schwarzwalder <jschwarzwalder@users.noreply.github.com>
2018-08-11 09:56:15 -0700

    Creating Abstract for Items informaiton

---
+++ Assets/Scripts/Item.cs
@@ -0,0 +1,26 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class Item : MonoBehaviour {
+
+    [SerializeField] string Name;
+    [SerializeField] int Value;
+    [SerializeField] string description;
+    [SerializeField] bool isSet;
+    [SerializeField] private ItemSets SetName;
+
+
+
+	// Use this for initialization
+	void Start () {
+		
+	}
+	
+	// Update is called once per frame
+	void Update () {
+		
+	}
+
+    
+}
