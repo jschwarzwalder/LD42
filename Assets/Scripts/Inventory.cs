8e0d5de1606c54eaaa738798736e1e53b79fa087
Jami Schwarzwalder <jschwarzwalder@users.noreply.github.com>
2018-08-11 11:22:56 -0700

    switching to using rows and colums instead of one inventory size

--- Assets/Scripts/Inventory.cs
+++ Assets/Scripts/Inventory.cs
@@ -2,42 +2,92 @@
 using System.Collections.Generic;
 using UnityEngine;
 
-public class Arrays : MonoBehaviour {
+public class Arrays : MonoBehaviour
+{
 
     private Item[] Items;
     private int inventoryScore;
-    [SerializeField] int InventorySize;
+    [SerializeField] int rows;
+    [SerializeField] int columns;
+    private int inventorySize;
 
-	// Use this for initialization
-	void Start () {
-        Items = new Item[InventorySize]; 
+    // Use this for initialization
+    void Start()
+    {
+        inventorySize = rows * columns;
+        Items = new Item[inventorySize];
         inventoryScore = 0;
-	}
-	
-	// Update is called once per frame
-	void Update () {
-		
-	}
+    }
 
-    void addItem (Item newItem, int index) {
-
-        if (Items[index] = null) {
-            Items[index] = newItem;
-        }     
+    // Update is called once per frame
+    void Update()
+    {
 
     }
 
-    void Score() {
-        
-        for(int i = 0 ; i <= InventorySize; i++) {
-            inventoryScore += Items[i].getValue();
+    void addItem(Item newItem, int row, int col)
+    {
+
+        int index = row * columns + col;
+
+        if (Items[index] == null)
+        {
+            Items[index] = newItem;
+        }
+
+    }
+
+    public Item getItemAt(int row, int col)
+    {
+        int index = row * columns + col;
+
+        return Items[index];
+
+    }
+
+    void scoreItem(Item newItem, int row, int col)
+    {
+        int index = row * columns + col;
+
+        ScoreAtIndex(index);
+    }
+
+    void ScoreAtIndex(int index)
+    {
+        Item item = Items[index];
+        if (!item.isInSet())
+        {
+            inventoryScore += item.getValue();
+        }
+        else
+        {
+            //TODO: handle set value
+            // inventoryScore += item.modScore();
         }
     }
 
-    void destroyItem(Item newItem, int index)
+    void ScoreAll()
     {
+
+        for (int i = 0; i <= inventorySize; i++)
+        {
+            ScoreAtIndex(i);
+        }
+
+    }
+
+    void destroyItem(Item newItem, int row, int col)
+    {
+        int index = row * columns + col;
+
         Items[index] = null;
 
-        addItem(newItem, index);
+        addItem(newItem, row, col);
+    }
+
+
+    public int getScore()
+    {
+        return inventoryScore;
     }
 }
