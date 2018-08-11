8f04254258ebcfdd4cbd4a0db3bc4bee102ba536
Jami Schwarzwalder <jschwarzwalder@users.noreply.github.com>
2018-08-11 09:56:46 -0700

    Creating an Enum to contain names of Item Sets

---
+++ Assets/Scripts/ItemSets.cs
@@ -0,0 +1,18 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class ItemSets : MonoBehaviour {
+
+    enum Sets { Wizard, Ranged, Warrior, }
+
+	// Use this for initialization
+	void Start () {
+		
+	}
+	
+	// Update is called once per frame
+	void Update () {
+		
+	}
+}
