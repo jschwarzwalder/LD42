27154518f00fe21178c711f788c484cbf5d08e32
Paul Schwarzwalder <pschwarz99@gmail.com>
2018-08-12 10:41:54 -0700

    Alert window now pauses game, and ok button closes it

---
+++ Assets/Scripts/AlertWindow.cs
@@ -0,0 +1,26 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class AlertWindow : MonoBehaviour {
+
+	// Use this for initialization
+	void Start () {
+	    Open();
+	}
+	
+	// Update is called once per frame
+	void Update () {
+		
+	}
+
+    public void Open () {
+        gameObject.SetActive(true);
+        Time.timeScale = 0;
+    }
+
+    public void Close () {
+        gameObject.SetActive(false);
+        Time.timeScale = 1;
+    }
+}
