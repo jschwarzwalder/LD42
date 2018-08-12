c830c157857653b4dae0bb35c4cef90bf98f4b53
Paul Schwarzwalder <pschwarz99@gmail.com>
2018-08-12 13:10:25 -0700

    Added Trash Can

---
+++ Assets/Scripts/Trash.cs
@@ -0,0 +1,31 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class Trash : MonoBehaviour {
+
+	// Use this for initialization
+	void Start () {
+		
+	}
+	
+	// Update is called once per frame
+	void Update () {
+
+    }
+
+    private void OnMouseDown()
+    {
+        GameObject cursorObj = GameObject.FindGameObjectWithTag("Cursor");
+        Cursor cursor = cursorObj.GetComponent<Cursor>();
+        Item item = cursor.SelectedItem;
+        if (item != null)
+        {
+            cursor.SelectedItem = null;
+            GameObject.Destroy(item.gameObject);
+            
+        }
+
+    }
+
+}
