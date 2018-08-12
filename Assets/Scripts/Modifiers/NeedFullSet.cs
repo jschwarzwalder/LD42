e900779c15c7fd8c3e1a77253cb375f25cec4cb0
Paul Schwarzwalder <pschwarz99@gmail.com>
2018-08-11 23:42:54 -0700

    Added NeedFullSet, SetBonus, and SetMax

---
+++ Assets/Scripts/Modifiers/NeedFullSet.cs
@@ -0,0 +1,31 @@
+﻿using UnityEngine;
+
+namespace Modifiers {
+    public class NeedFullSet : ItemModifier {
+
+        public int SetSize;
+
+        public override int ValueMultiplier {
+            get { return 5; }
+        }
+
+        public override int GetSetValue (Scorepad scorepad) {
+            Item.ItemSet? set = GetComponent<Item>().getSet();
+            int value = GetComponent<Item>().Value;
+            if (set == null) {
+                return SetSize <= 1 ? value : 0;
+            }
+
+            scorepad.IncrementSetCount(set.Value);
+            scorepad.IncreaseSetValue(set.Value, value);
+
+            if (scorepad.GetSetCount(set.Value) < SetSize) {
+                return 0;
+            }
+
+            int setValue = scorepad.GetSetValue(set.Value);
+            scorepad.ResetSet(set.Value);
+            return setValue;
+        }
+    }
+}
---
+++ Assets/Scripts/Modifiers/SetBonus.cs
@@ -0,0 +1,32 @@
+﻿using UnityEngine;
+
+namespace Modifiers {
+    public class SetBonus : ItemModifier {
+
+        public int SetSize;
+        public int SetMultiplier;
+
+        public override int ValueMultiplier {
+            get { return 2; }
+        }
+
+        public override int GetSetValue (Scorepad scorepad) {
+            Item.ItemSet? set = GetComponent<Item>().getSet();
+            int value = GetComponent<Item>().Value;
+            if (set == null) {
+                return SetSize <= 1 ? value * SetMultiplier : value;
+            }
+
+            scorepad.IncrementSetCount(set.Value);
+            scorepad.IncreaseSetValue(set.Value, value);
+
+            if (scorepad.GetSetCount(set.Value) < SetSize) {
+                return value;
+            }
+
+            int setValue = scorepad.GetSetValue(set.Value);
+            scorepad.ResetSet(set.Value);
+            return setValue + value;
+        }
+    }
+}
