471f51c5f3d0e9ea81aadbd3023a55e756b1aa6a
Paul Schwarzwalder <pschwarz99@gmail.com>
2018-08-11 20:50:26 -0700

    3 Arrows are now required for ranged set

--- Assets/Scripts/Scorepad.cs
+++ Assets/Scripts/Scorepad.cs
@@ -4,7 +4,7 @@ using System.Linq;
 using System.Text;
 
 public class Scorepad {
-    private const int ArrowsNeeded = 2;
+    private const int ArrowsNeeded = 3;
     private const int BowsNeeded = 1;
 
     public int Arrows { get; set; }
