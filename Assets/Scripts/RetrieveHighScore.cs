8f9006fdc3e6ec33af6bb4904fa7b203be69e040
Jami Schwarzwalder <jschwarzwalder@users.noreply.github.com>
2018-08-28 00:13:59 -0700

    Display Player's Highest Score on Level Select Scence

---
+++ Assets/Scripts/RetrieveHighScore.cs
@@ -0,0 +1,39 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.UI;
+
+public class RetrieveHighScore : MonoBehaviour {
+
+    int levelScore;
+    [SerializeField] Text ScoreDisplay;
+
+	// Use this for initialization
+	void Start () {
+
+        LevelSelectButton levelButton =  GetComponentInParent<LevelSelectButton>();
+        //Get Level Number
+        int level = levelButton.LevelIndex;
+
+        //Look up Score
+        levelScore = PlayerPrefs.GetInt("Level" + level);
+
+        //Display Score
+        if (levelScore > 1)
+        {
+            ScoreDisplay.text = "High Score: " + levelScore;
+        }
+        else
+        {
+            ScoreDisplay.text = "Unplayed";
+        }
+       
+
+		
+	}
+	
+	// Update is called once per frame
+	void Update () {
+		
+	}
+}
